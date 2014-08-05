013b2b1db153788fd259fa85f926af3fbff85631
kylefdoherty <kyledoherty22@gmail.com>
2014-08-05 15:24:49 -0400

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805190453) do
+ActiveRecord::Schema.define(version: 20140805150647) do
 
   create_table "line_items", force: true do |t|
     t.integer  "item_id"
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 20140805190453) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
   end
 
 end
