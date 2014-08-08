8eeff6bb270fae8e9501cdc2e5a1574bffb139b0
Emily Lowing <elowing@gmail.com>
2014-08-08 12:05:39 -0400

    still working on heroku errors

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   $('#product_tag_tag_id').change(function(){
     $tagId = $(this).find('option:selected').val(),
     $products = $('p');
-    debugger
     // if ($products)
     // display only '.tag'+tagId
   });
