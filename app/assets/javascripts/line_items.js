0b78f03480abdf5d362be617cf9b14e3b25a15b2
Emily Lowing <elowing@gmail.com>
2014-08-12 18:35:45 -0400

    committing changes before pull

--- app/assets/javascripts/line_items.js
+++ app/assets/javascripts/line_items.js
@@ -1,7 +1,11 @@
 $(function(){
 
-
   $('.edit_line_item').change(function(){
+    var $updatedQuant = $(this).find('option:selected').val();
+    if ($updatedQuant === '0') {
+      debugger
+      $('#line_item_<%=j "#{@line_item.id}" %>').remove();
+    } end
     $(this).submit();
   });
 
--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#product_tag_tag_id').change(function(){
-    $tagId = $(this).find('option:selected').val(),
-    $productsToDisplay = $('h5.tag'+$tagId).closest('.item').parent();
+    var $tagId = $(this).find('option:selected').val(),
+        $productsToDisplay = $('h5.tag'+$tagId).closest('.item').parent();
     $('.display').children().hide();
     if ($productsToDisplay.length === 0) {
       $('.display').children().show();
