66d80e9bb3ccdbc3fc15e03e7d88cd75d072e507
kylefdoherty <kyledoherty22@gmail.com>
2014-08-21 10:39:48 -0400

    precompiled assets locally

---
+++ public/assets/line_items-40e742e8873efb3ed343e8ec45c62bf7.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  var hello = "cruel world";
+
+  $('.edit_line_item').change(function(){
+    var $updatedQuant = $(this).find('option:selected').val();
+    if ($updatedQuant === '0') {
+      $(this).parent().siblings().last().find('form').submit();
+    } else {
+      $(this).submit();
+    }
+  });
+
+});
