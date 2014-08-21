c998292c5dcd075cb56fc88b692ce76ad27673b0
kylefdoherty <kyledoherty22@gmail.com>
2014-08-21 10:21:04 -0400

    status

--- app/assets/javascripts/line_items.js
+++ app/assets/javascripts/line_items.js
@@ -1,5 +1,7 @@
 $(function(){
 
+  var hello = "cruel world";
+
   $('.edit_line_item').change(function(){
     var $updatedQuant = $(this).find('option:selected').val();
     if ($updatedQuant === '0') {
